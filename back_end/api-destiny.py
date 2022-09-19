38f74609f8fa77f41404512ee7770d10294af1d3
Kasper Holm <kasper.marhauer@gmail.com>
2022-09-19 20:38:58 +0200

    Comments and readability

--- back_end/api-destiny.py
+++ back_end/api-destiny.py
@@ -1,9 +1,11 @@
+from genericpath import isfile
 import requests
 from requests_oauthlib import OAuth2Session
 from dotenv import load_dotenv
 import os
 import json
 
+#load environmental variables
 load_dotenv()
 
 #authentication variables
@@ -21,52 +23,72 @@ get_user_details_endpoint = None
 #session
 session = OAuth2Session(client_id=client_id, redirect_uri=redirect_url)
 
-#all requests need an api key
-additional_headers = {'X-API-KEY': api_key, 'Content-Type': 'application/x-www-form-urlencoded',
-                    'Authorization': f'Bearer {token_dict}'}
+#additional headers required for every request sent to the API
+HEADERS = {'X-API-KEY': api_key,
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                        'Authorization': f'Bearer {token_dict}'}
 
 def main():
-    response = session.get(url=get_user_details_endpoint, headers=additional_headers)
+    response = session.get(url=get_user_details_endpoint, headers=HEADERS)
     if response.reason != 'OK':
         print(f"Authorization failed: {response.status_code}\n{response.reason}")
         response = response
 
+    #write response to terminal and save it to a local file * TEMPORARY *
     print(f"RESPONSE STATUS: {response.status_code}")
     print(f"RESPONSE REASON: {response.reason}")
     print(f"REPONSE TEXT: \n{response.text}")
     open('data.json', 'a').write(response.text)
 
 def get_user_details():
+    '''
+    Use this function to get relevant user deatails, such as:\n
+    Membership ID,\n
+    Membership Type,\n
+    as well as others.\n
+
+    This data is used in a lot of GET & POST endpoints relevant only to a specific account.
+    '''
     user_details_endpoint = 'https://www.bungie.net/Platform/User/GetMembershipsForCurrentUser/'
-    response = session.get(url=user_details_endpoint, headers=additional_headers)
+    response = session.get(url=user_details_endpoint, headers=HEADERS)
     if response.reason != 'OK':
-        print(f'Auth failed. Click link below to authorize')
         get_token()
-        current_token = open('token.txt', 'r').readline()
-        additional_headers['Authorization'] =  f'Basic {current_token}'
         response = response
+    
+    #dump json response to a local file * TEMPORARY *
     json_object = json.dumps(response.json(), indent=2)
     open('data.json', 'a').write(json_object)
 
 def get_token():
-    current_token = open('token.txt', 'r').readline()
+    '''
+    Stores an auth token in a local file, "token.txt"
+    '''
+    if os.path.isfile('token.txt'):
+        current_token = open('token.txt', 'r').readline()
+    
+    print(f'Auth failed. Click link below to authorize')
     auth_link = session.authorization_url(base_auth_url)
     print(f"Auth link: {auth_link[0]}")
 
     #auth returns a code used in retrieving auth token
     redirect_response = input("Insert redirect url incl. params: ")
+
+    #returns an auth token
     token_dict = session.fetch_token(
         include_client_id=True,
         client_id=client_id,
         token_url=token_url,
         authorization_response=redirect_response,
     )
-    if token_dict['access_token'] != current_token:
+    
+    #if a new token has been generated, store it locally * TEMPORARY *
+    if token_dict['access_token'] != current_token and current_token != None:
         open('token.txt', 'w').write(token_dict['access_token'])
 
-# main()
+if __name__ == '__main__':
+    get_user_details()
 
-get_user_details()
+    #main()
 
 
 def mainfest_download():
