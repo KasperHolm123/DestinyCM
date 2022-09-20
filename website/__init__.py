4e1b7ea1e92d90da1de3fda1599a0cb9921101eb
Kasper Holm <kasper.marhauer@gmail.com>
2022-09-20 18:52:11 +0200

    view template and index view

--- website/__init__.py
+++ website/__init__.py
@@ -4,4 +4,8 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'afoiaefj9823479oijiosgp98q34pahgrjenfdkæzkhg74'
 
+    from .views import views
+
+    app.register_blueprint(views, url_prefix='/')
+
     return app
